e9175b8da6cdf7a0e3e70f6bf281899d2a48e378
Jose Manuel Ojeda <manu_ojeda@hotmail.com>
2020-03-31 07:52:30 -0700

    LoadFromXpoObject

---
+++ Class1.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.ML;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BIT.Xpo.MSML
+{
+    public class ConsumeModel
+    {
+
+
+      
+        // For more info on consuming ML.NET models, visit https://aka.ms/model-builder-consume
+        // Method for consuming model in your app
+        //public static ModelOutput Predict(ModelInput input)
+        //{
+
+        //    // Create new MLContext
+        //    MLContext mlContext = new MLContext();
+
+        //    // Load model & create prediction engine
+        //    string modelPath = @"C:\Users\Joche\AppData\Local\Temp\MLVSTools\ConsoleApp1ML\ConsoleApp1ML.Model\MLModel.zip";
+        //    ITransformer mlModel = mlContext.Model.Load(modelPath, out var modelInputSchema);
+        //    var predEngine = mlContext.Model.CreatePredictionEngine<ModelInput, ModelOutput>(mlModel);
+
+        //    // Use model to make prediction on input data
+        //    ModelOutput result = predEngine.Predict(input);
+        //    return result;
+        //}
+    }
+
+
+}
---
+++ DataOperationsExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.Xpo;
+using Microsoft.ML;
+using System;
+
+namespace BIT.Xpo.MSML
+{
+    public static class DataOperationsExtensions
+    {
+        public static IDataView LoadFromXpoObject(this DataOperationsCatalog Instance,Session session,Type ObjectType,string Properties,CriteriaOperator Criteria,string TextPropertyName,string LabelPropertyName)
+        {
+            DevExpress.Xpo.XPView View = new DevExpress.Xpo.XPView(session, ObjectType, Properties, Criteria);
+            return new XpoInputObjectDataView(View, TextPropertyName, LabelPropertyName);
+        }
+    }
+}
